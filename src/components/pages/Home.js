4710f3247d1f50945b5a1be38af64a89e1b3a24a
Mariana Markevych <104388688+MarianaMarkevych@users.noreply.github.com>
2022-07-13 16:45:43 +0300

    initial commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,36 +4,105 @@ import PreSection from "../PreSection";
 import Houses from "../Houses";
 import Footer from "../Footer";
 import "../Houses.css";
+import { useState } from "react";
+import Select from "../Select";
 
 function Home() {
-  // useEffect(() => {
-  //   console.log(label);
-  //   // setSelected(
-  //   //   label === "Фільтр" ? posts : posts.filter((post) => post.label === label)
-  //   // );
-  // }, [label]);
+  const labels = ["В проєкті", "Будується", "Зданий в експлуатацію"];
+  const text = ["Parus", "Avalon"];
+  const [selected, setSelected] = useState("Фільтр");
+  const [filteredResults, setFilteredResults] = useState([]);
 
-  //   if (option === "В проєкті") {
-  //     const filtered = posts.filter((post) => post.label === "В проєкті");
-  //     setPosts(filtered);
-  //     console.log(filtered);
-  //   } else if (option === "Будується") {
-  //     const filtered = posts.filter((post) => post.label === "Будується");
-  //     setPosts(filtered);
-  //     console.log(filtered);
-  //   } else if (option === "Зданий в експлуатацію") {
-  //     const filtered = posts.filter(
-  //       (post) => post.label === "Зданий в експлуатацію"
-  //     );
-  //     setPosts(filtered);
-  //     console.log(filtered);
+  // // варіант 1
+  const sortedResults = React.useEffect(() => {
+    const filterResults = posts.filter((post) => post.label === selected);
+    setFilteredResults(filterResults);
+    console.log(filterResults);
+  }, [selected]);
+
+  const searchItems = (label) => {
+    setSelected(label);
+    console.log(label);
+  };
+  // // варіант 2
+  // const searchItems = (label) => {
+  //   setSelected(label);
+  //   console.log(label);
+  // };
+
+  //   if (selected !== "") {
+  //     const filteredPost = [...posts].filter((post) => post.label === label);
+  //     setFilteredResults(filteredPost);
+  //     console.log(filteredPost);
+  //   } else {
+  //     setFilteredResults(posts);
+  //     console.log(posts);
   //   }
   // };
 
+  // const developerFiltered = (text) => {
+  //   setSelected(text);
+  //   const devfiltered =
+  //     text === "Всі" ? posts : posts.filter((post) => post.text === text);
+  //   setPosts(devfiltered);
+  // };
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      src: "images/img-1.jpg",
+      text: "Parus Smart",
+      label: "Зданий в експлуатацію",
+    },
+    {
+      id: 2,
+      src: "images/img-2.jpg",
+      text: "Parus Premium",
+      label: "В проєкті",
+    },
+    {
+      id: 3,
+      src: "images/img-3.jpg",
+      text: "Parus City",
+      label: "Будується",
+    },
+    {
+      id: 4,
+      src: "images/img-4.jpg",
+      text: "Avalon Smart",
+      label: "Зданий в експлуатацію",
+    },
+    {
+      id: 5,
+      src: "images/img-5.jpg",
+      text: "Avalon Premium",
+      label: "Будується",
+    },
+    {
+      id: 6,
+      src: "images/img-6.jpg",
+      text: "Avalon Gardens",
+      label: "Будується",
+    },
+  ]);
   return (
     <>
       <PreSection />
-      <Houses title="Новобудови Львова" />
+      <h1 className="house--text">Новобудови Львова</h1>
+      <span className="house--select">
+        <Select
+          options={labels}
+          title="Cтан будівництва:"
+          // defaultValue="Фільтр"
+          setSelected={searchItems}
+          selected="Фільтр"
+        />
+        <Select
+          options={text}
+          selected="Забудовники"
+          setSelected={setSelected}
+        />
+      </span>
+      <Houses posts={posts} />
       <Footer />
     </>
   );
